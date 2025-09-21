fa4ac6e316b978024ca55d95ed805ea095252903
DiUnnown | DUNW <whotie506@gmail.com>
2025-09-21 10:01:31 +0900

    05_practice_2.ino, some fixes.

--- 05_practice_2.ino
+++ 05_practice_2.ino
@@ -7,17 +7,13 @@ void setup() {
 void loop() {
   digitalWrite(PIN_LED, 0);
   delay(1000);
-  digitalWrite(PIN_LED, 1);
-  delay(100);
-  for(int i = 0; i < 10; i++){
+  
+  for(int i = 1; i <= 10; i++){
     digitalWrite(PIN_LED, i%2);
     delay(100);
   }
   
   digitalWrite(PIN_LED, 1);
 
-  while(1){
-    
-  }
-  
+  while(1){  }
 }
